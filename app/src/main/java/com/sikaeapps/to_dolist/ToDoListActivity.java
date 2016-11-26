366cf4e13dc1da350acc1c7e1cc8c55a547f9389
Sikae <sikabarca@gmail.com>
2016-11-25 23:24:18 -0500

    feat: Show todo and done items on list views

--- app/src/main/java/com/sikaeapps/to_dolist/ToDoListActivity.java
+++ app/src/main/java/com/sikaeapps/to_dolist/ToDoListActivity.java
@@ -6,15 +6,51 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ToDoListActivity extends AppCompatActivity {
+
+    @BindView(R.id.to_do_list_view)
+    ListView toDoListView;
+    @BindView(R.id.done_list_view)
+    ListView doneListView;
+
+    private ItemManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
+        ButterKnife.bind(this);
+        manager = new ItemManager();
+
+        if (manager.getToDoItems().isEmpty() && manager.getDoneItems().isEmpty()) {
+            try {
+                manager.addItem(new Item(
+                        "TODO Title Sample",
+                        "TODO Description Sample",
+                        "TODO Location Sample"));
+
+                manager.addItem(new Item(
+                        "DONE Title Sample",
+                        "DONE Description Sample",
+                        "DONE Location Sample"));
+
+                manager.checkItemAtIndex(1);
+
+            } catch (DuplicatedItemException e) {
+
+            }
+        }
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        toDoListView.setAdapter(new ItemAdapter(this, manager.getToDoItems()));
+        doneListView.setAdapter(new ItemAdapter(this, manager.getDoneItems()));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {

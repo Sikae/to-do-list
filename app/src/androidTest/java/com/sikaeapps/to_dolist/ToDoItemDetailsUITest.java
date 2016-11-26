1b73e4e649e9a3319347690536b27f64e5960efb
m <lmiguelvargasf@gmail.com>
2016-11-26 12:55:54 -0500

    test: Test ui elements are shown
    Verify that after user clicks on an item, its details are shown in a new activity.

---
+++ app/src/androidTest/java/com/sikaeapps/to_dolist/ToDoItemDetailsUITest.java
@@ -0,0 +1,32 @@
+package com.sikaeapps.to_dolist;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+
+public class ToDoItemDetailsUITest {
+
+    @Rule
+    public ActivityTestRule<ToDoListActivity> toDoListActivityTestRule =
+            new ActivityTestRule<>(ToDoListActivity.class);
+
+    @Test
+    public void whenUserClicksOnToDoItemThenItemDetailsAreShownInNewActivity() {
+        Item item = toDoListActivityTestRule.getActivity().getManager().getItemAtIndex(0);
+        onData(anything()).inAdapterView(withId(R.id.to_do_list_view)).atPosition(0).perform(click());
+        onView(withId(R.id.item_title_details)).check(matches(withText(item.getTitle())));
+        onView(withId(R.id.item_description_details)).check(matches(withText(item.getDescription())));
+        onView(withId(R.id.item_location_details)).check(matches(withText(item.getLocation())));
+    }
+
+
+}

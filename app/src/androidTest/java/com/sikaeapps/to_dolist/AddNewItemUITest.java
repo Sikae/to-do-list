96c8f231c15e615f6cf4ab20de869e38ee69212b
m <lmiguelvargasf@gmail.com>
2016-11-26 21:48:34 -0500

    test: Test a duplicated item is not added twice

--- app/src/androidTest/java/com/sikaeapps/to_dolist/AddNewItemUITest.java
+++ app/src/androidTest/java/com/sikaeapps/to_dolist/AddNewItemUITest.java
@@ -11,11 +11,14 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 public class AddNewItemUITest {
 
@@ -42,10 +45,7 @@ public class AddNewItemUITest {
 
     @Test
     public void whenUserSavesANewItemThenItemIsDisplayedOnToDoItemList() throws Exception {
-        onView(withId(R.id.new_edit_item_title)).perform(typeText("Do the laundry"));
-        onView(withId(R.id.new_edit_item_description)).perform(typeText("Do the laundry at home at 9.00 am"));
-        onView(withId(R.id.new_edit_item_location)).perform(typeText("Home"));
-
+        entryDoTheLaundryTask();
         onView(withId(R.id.save_button)).perform(click());
 
         onData(anything()).inAdapterView(withId(R.id.to_do_list_view)).atPosition(1).
@@ -56,4 +56,21 @@ public class AddNewItemUITest {
                 onChildView(withId(R.id.item_location)).
                 check(matches(withText("Home")));
     }
+
+    @Test
+    public void whenUserTriesToAddADuplicatedItemThenAToastMessageIsDisplayed() {
+        entryDoTheLaundryTask();
+        onView(withId(R.id.save_button)).perform(click());
+        onView(withId(R.id.fab)).perform(click());
+        entryDoTheLaundryTask();
+        onView(withId(R.id.save_button)).perform(click());
+        onView(withText(R.string.duplicated_item_message)).inRoot(withDecorView(not(is(toDoListActivityTestRule.getActivity().getWindow().getDecorView())))).check(matches(isDisplayed()));
+    }
+
+    private void entryDoTheLaundryTask() {
+        onView(withId(R.id.new_edit_item_title)).perform(typeText("Do the laundry"));
+        onView(withId(R.id.new_edit_item_description)).perform(typeText("Do the laundry at home at 9.00 am"));
+        onView(withId(R.id.new_edit_item_location)).perform(typeText("Home"));
+    }
+
 }

64a24df235dcc5cb1ff3007dd8c9de381b9ffa49
m <lmiguelvargasf@gmail.com>
2016-11-26 21:41:16 -0500

    test: Test user can edit an item
    Test that user can edit an item and save it.

--- app/src/androidTest/java/com/sikaeapps/to_dolist/EditToDoUITest.java
+++ app/src/androidTest/java/com/sikaeapps/to_dolist/EditToDoUITest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withHint;
@@ -35,4 +36,31 @@ public class EditToDoUITest {
         onView(withId(R.id.new_edit_item_location)).check(matches(withHint(R.string.location_hint)));
         onView(withId(R.id.save_button)).check(matches(withText(R.string.save)));
     }
+
+    @Test
+    public void givenUserEditsAToDoItemWhenPressingSaveItemIsUpdatedOnToDoItemLlist() {
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.new_edit_item_title)).perform(typeText("Title"));
+        onView(withId(R.id.new_edit_item_description)).perform(typeText("Description..."));
+        onView(withId(R.id.new_edit_item_location)).perform(typeText("Location"));
+        onView(withId(R.id.save_button)).perform(click());
+
+        onData(anything()).inAdapterView(withId(R.id.to_do_list_view)).atPosition(1).perform(click());
+        onView(withId(R.id.edit_button)).perform(click());
+
+        onView(withId(R.id.new_edit_item_title)).perform(replaceText("Do the laundry"));
+        onView(withId(R.id.new_edit_item_description)).perform(replaceText("Do the laundry at home at 9.00 am"));
+        onView(withId(R.id.new_edit_item_location)).perform(replaceText("Home"));
+
+        onView(withId(R.id.save_button)).perform(click());
+
+        onData(CoreMatchers.anything()).inAdapterView(withId(R.id.to_do_list_view)).atPosition(1).
+                onChildView(withId(R.id.item_title)).
+                check(matches(withText("Do the laundry")));
+
+        onData(CoreMatchers.anything()).inAdapterView(withId(R.id.to_do_list_view)).atPosition(1).
+                onChildView(withId(R.id.item_location)).
+                check(matches(withText("Home")));
+    }
+
 }

210e9d9742fb56cdc338db0bb77d1c2d3dff1e05
Sikae <sikabarca@gmail.com>
2016-11-27 19:00:51 -0500

    test: Test swipe in todo list
    When user swipes a todo item, test that options are displayed.

--- app/src/androidTest/java/com/sikaeapps/to_dolist/ToDoListUITest.java
+++ app/src/androidTest/java/com/sikaeapps/to_dolist/ToDoListUITest.java
@@ -5,11 +5,15 @@ import android.support.test.rule.ActivityTestRule;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
 
 public class ToDoListUITest {
 
@@ -25,5 +29,13 @@ public class ToDoListUITest {
         onView(withId(R.id.done_text_view)).check(matches(isDisplayed()));
         onView(withId(R.id.done_text_view)).check(matches(withText(R.string.done)));
         onView(withId(R.id.done_list_view)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void whenUserSwipesToDoItemThenOptionsAreDisplayed() throws Exception {
+        Item item = toDoListActivityTestRule.getActivity().getManager().getItemAtIndex(0);
+        onData(anything()).inAdapterView(withId(R.id.to_do_list_view)).atPosition(0).perform(swipeLeft());
+        onView(withId(R.id.delete_button)).check(matches(isDisplayed()));
+        onView(withId(R.id.check_button)).check(matches(isDisplayed()));
     }
 }

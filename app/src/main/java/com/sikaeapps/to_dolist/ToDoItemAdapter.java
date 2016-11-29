877c3e524b05b120605ab7fe63c63612f83c5523
Sikae <sikabarca@gmail.com>
2016-11-29 17:58:50 -0500

    fix: Fix ui tests
    Fix error when user taps on a todo item to see details.

--- app/src/main/java/com/sikaeapps/to_dolist/ToDoItemAdapter.java
+++ app/src/main/java/com/sikaeapps/to_dolist/ToDoItemAdapter.java
@@ -1,25 +1,34 @@
 package com.sikaeapps.to_dolist;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
 import com.chauthai.swipereveallayout.SwipeRevealLayout;
 import com.chauthai.swipereveallayout.ViewBinderHelper;
-
 import java.util.List;
 
-public class ToDoItemAdapter extends ArrayAdapter {
+import static com.sikaeapps.to_dolist.ToDoListActivity.EDIT_ITEM_REQUEST_CODE;
 
+
+public class ToDoItemAdapter extends ArrayAdapter {
     private final LayoutInflater inflater;
     private final ViewBinderHelper binderHelper;
     private List<Item> items;
+    private AdapterView.OnItemClickListener onItemClickListener;
+    private ItemManager manager;
+    private Activity activity;
 
-    public ToDoItemAdapter(Context context, List<Item> items) {
+    public ToDoItemAdapter(Context context, List<Item> items, ItemManager manager, Activity activity) {
         super(context, R.layout.to_do_item_layout, items);
+        this.activity = activity;
+        this.manager = manager;
         this.items = items;
         inflater = LayoutInflater.from(context);
         binderHelper = new ViewBinderHelper();
@@ -28,12 +37,13 @@ public class ToDoItemAdapter extends ArrayAdapter {
     private class ViewHolder {
         TextView titleTextView;
         TextView locationTextView;
+        View toDoItemListView;
         View deleteView;
         SwipeRevealLayout swipeLayout;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
 
         if (convertView == null) {
@@ -44,31 +54,27 @@ public class ToDoItemAdapter extends ArrayAdapter {
             holder.locationTextView = (TextView) convertView.findViewById(R.id.item_location);
             holder.deleteView = convertView.findViewById(R.id.delete_layout);
             holder.swipeLayout = (SwipeRevealLayout) convertView.findViewById(R.id.swipe_layout);
-
+            holder.toDoItemListView = convertView.findViewById(R.id.to_do_item_list_view);
             convertView.setTag(holder);
         } else {
             holder = (ViewHolder) convertView.getTag();
         }
 
-//        final String item = (String) getItem(position);
-//        if (item != null) {
-//            binderHelper.bind(holder.swipeLayout, item);
-//
-//            holder.titleTextView.setText(items.get(position).getTitle());
-//            holder.locationTextView.setText(items.get(position).getLocation());
-//            holder.deleteView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    remove(item);
-//                }
-//            });
-//        }
+        holder.toDoItemListView.setOnClickListener(new View.OnClickListener() {
+        @Override
+            public void onClick(View v) {
+            Log.d("MESSAGE", "ON CLICK IN ITEM LIST VIEW" + position);
+            Intent intent = new Intent(getContext(), ItemDetailsActivity.class);
+            Item item = manager.getItemAtIndex(position);
+            intent.putExtra(Constants.MANAGER, manager);
+            intent.putExtra(Constants.ITEM, item);
+            activity.startActivityForResult(intent, EDIT_ITEM_REQUEST_CODE);
+            }
+        });
 
         holder.titleTextView.setText(items.get(position).getTitle());
         holder.locationTextView.setText(items.get(position).getLocation());
-//
+
         return convertView;
     }
-
-
 }
